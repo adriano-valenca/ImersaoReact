907501d7f4a79123e62c0d0d958279fba8477761
ADRIANO VALENCA DA COSTA <64167395+adriano-valenca@users.noreply.github.com>
2020-07-27 11:31:24 -0300

    Commit de teste

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      Hello World!
+      Alguma coisa aí no meio
+      update automático (Hot reload)
+      Teste
+    </div>
+  );
+}
+
+export default App;
